f5742939eb40ce4fde1a9c82db4f47b831d4281f
raz sherf <razsherf@gmail.com>
2022-12-12 20:41:51 +0200

    first commit for docker project

---
+++ app.py
@@ -0,0 +1,20 @@
+import sys
+from pathlib import Path
+path = Path(__file__).parent.parent  # i.e. the folder above the tests folder, which has app
+sys.path.append(path)
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+
+@app.route('/test')
+def say_hello():
+    return('Another Dir for the flask app   ')
+
+if __name__ == '__main__':
+    app.run()
